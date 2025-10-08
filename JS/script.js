90547afb8b65c3bb155ec62ab4f33a487df3d46e
Gorkem387 <gorkemyildiz7ds@gmail.com>
2025-10-08 10:26:25 +0200

    modif dans le javascript, rechercher par titre fonctionnel

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 let researchButton = document.getElementById("research-button");
 let typeInput = document.getElementById("type");
 let parameterInput = document.getElementById("parametre-input");
-const animeList = document.getElementById('anime-list');
+const animeList = document.getElementById('anime-card');
 
 async function fetchAnime() {
     const url = `https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=${parameterInput.value}&${typeInput.value}=${parameterInput.value}&sortBy=ranking&sortOrder=asc`;
@@ -19,14 +19,26 @@ async function fetchAnime() {
         const data = await response.json(); // Get JSON result
 
         animeList.innerHTML = data.data.map(anime => `
+            <style>
+                .anime-card h3, p, img {
+                    margin-bottom: 14px;
+                }
+                hr {
+                    border: none;
+                    height: 1px;
+                    background-color: #000;
+                    margin: 20px 0;
+                }
+            </style>
             <div class="anime-card">
                 <h3>${anime.title}</h3>
                 <img src="${anime.image}" alt="${anime.title}" />
-                <p><strong>Synopsis : </strong>${anime.synopsis}<br></p>
-                <span><strong>Genres : </strong>${anime.genres}<br></span>
-                <span><strong>Classement : </strong>${anime.ranking}<br></span>
-                <span><strong>Type : </strong>${anime.type}<br></span>
-                <span><strong>Nombre d'épisodes : </strong>${anime.episodes}<br></span>
+                <p><strong>Synopsis : </strong>${anime.synopsis}</p>
+                <p><strong>Genres : </strong>${anime.genres}</p>
+                <p><strong>Classement : </strong>${anime.ranking}</p>
+                <p><strong>Type : </strong>${anime.type}</p>
+                <p><strong>Nombre d'épisodes : </strong>${anime.episodes}</p>
+                <hr>
             </div>
         `).join('');
     } catch (error) {
