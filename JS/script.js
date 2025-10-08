28aaad56f3d5e3b23069fbe21166ac949e97da56
Gorkem387 <gorkemyildiz7ds@gmail.com>
2025-10-08 11:20:24 +0200

    dark mode

--- JS/script.js
+++ JS/script.js
@@ -52,6 +52,35 @@ async function fetchAnime() {
     }
 }
 
+(function () {
+
+    const root = document.documentElement;
+  
+    function toggleDarkMode() {
+      const currentTheme = root.getAttribute("data-theme");
+      const newTheme = currentTheme === "light" ? "dark" : "light";
+      root.setAttribute("data-theme", newTheme);
+      localStorage.setItem("theme", newTheme);
+    }
+  
+    function init() {
+      const storedPreference = localStorage.getItem("theme");
+      const systemPrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+      const theme = storedPreference || (systemPrefersDark ? "dark" : "light");
+      root.setAttribute("data-theme", theme);
+    }
+  
+    init();
+  
+    document.addEventListener("DOMContentLoaded", function () {
+      const togglers = document.querySelectorAll("[data-theme-toggler]");
+      togglers.forEach((toggler) => {
+        toggler.addEventListener("click", toggleDarkMode);
+      });
+    });
+  
+  })();
+
 researchButton.addEventListener('click', () => {
     fetchAnime();
 })
