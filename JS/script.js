3c0872844b58926689b01babda49d6fe4b666aaa
Oryx87 <baudin.mylan@laposte.net>
2025-10-09 17:48:24 +0200

    sessionStorage darkMode

--- JS/script.js
+++ JS/script.js
@@ -137,31 +137,6 @@ async function fetchAnime() {
   
   })();
 
-    function toggleDarkMode() {
-        const currentTheme = root.getAttribute("data-theme");
-        const newTheme = currentTheme === "light" ? "dark" : "light";
-        root.setAttribute("data-theme", newTheme);
-        localStorage.setItem("theme", newTheme);
-    }
-
-    function init() {
-        const storedPreference = localStorage.getItem("theme");
-        const systemPrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-        const theme = storedPreference || (systemPrefersDark ? "dark" : "light");
-        root.setAttribute("data-theme", theme);
-    }
-
-    init();
-
-    document.addEventListener("DOMContentLoaded", function () {
-        const togglers = document.querySelectorAll("[data-theme-toggler]");
-        togglers.forEach((toggler) => {
-            toggler.addEventListener("click", toggleDarkMode);
-        });
-    });
-
-})();
-
 document.addEventListener("DOMContentLoaded", () => {
     const selectType = document.getElementById("type");
     const inputText = document.getElementById("parametre-input");
